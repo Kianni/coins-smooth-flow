c2d81da4f66a1ecbe9ec86aa00e6fd6c12061bae
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-05-21 19:00:25 +0300

    first data and UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  data = [
+    {
+      payer: 'София',
+      asset: 'Вакцина от клещевого энцефалита',
+      cost: 55,
+      date: '21.5',
+    },
+    { payer: 'София', asset: 'Лекарства от аллергии', cost: 55, date: '11.5' },
+    { payer: 'Кирилл', asset: 'Кроссовки', cost: 88, date: '9.5' },
+  ];
+
+  getDebt() {
+    let expenditure_Kirill = 0;
+    let expenditure_Sofia = 0;
+  }
   title = 'coins-smooth-flow';
 }
