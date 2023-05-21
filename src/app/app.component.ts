294eeb33466d8b9dc19c41b52d59cedcc19a35c3
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-05-21 19:23:11 +0300

    compute debt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,31 @@ export class AppComponent {
     { payer: 'София', asset: 'Лекарства от аллергии', cost: 55, date: '11.5' },
     { payer: 'Кирилл', asset: 'Кроссовки', cost: 88, date: '9.5' },
   ];
+  title = 'coins-smooth-flow';
+  debtAmount: number = 0;
+  debtor: string = '';
+  sentence: string = '';
+
+  ngOnInit() {
+    this.getDebt();
+  }
 
   getDebt() {
     let expenditure_Kirill = 0;
     let expenditure_Sofia = 0;
+    this.data.forEach((asset) => {
+      if (asset.payer === 'София') expenditure_Sofia += asset.cost;
+      if (asset.payer === 'Кирилл') expenditure_Kirill += asset.cost;
+    });
+    this.debtor =
+      expenditure_Sofia / 2 > expenditure_Kirill / 2 ? 'Кирилл' : 'София';
+    this.sentence = this.makeSentence();
+    this.debtAmount = Math.abs(expenditure_Sofia / 2 - expenditure_Kirill / 2);
   }
-  title = 'coins-smooth-flow';
+
+  makeSentence() {
+    return this.debtor === 'Кирилл'
+      ? 'Кирилл должен перевести Софии '
+      : 'Cофия должна перевести Кириллу ';
+  }
 }
