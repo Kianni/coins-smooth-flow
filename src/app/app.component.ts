fe068c57c82adec061165567487ff3a35c3208b4
Kirill Nikolaev <nikolaeff.k.a@gmail.com>
2023-07-17 20:26:49 +0300

    add morda tremolo y zoomolo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,14 @@ export class AppComponent {
   firestore: Firestore = inject(Firestore);
   spendings$: Observable<any[]>;
   aCollection: CollectionReference;
+  _blackBackground = false;
+
+  get bg() {
+    return this._blackBackground;
+  }
+  set bg(value: boolean) {
+    this._blackBackground = value;
+  }
 
   constructor() {
     this.aCollection = collection(this.firestore, 'spendings');
@@ -52,6 +60,11 @@ export class AppComponent {
 
   onLoginCheck(data: boolean) {
     this.loggedIn = data;
+    data ? (this.bg = false) : null;
+  }
+
+  onHover() {
+    this.bg = !this.bg;
   }
 
   ngOnDestroy() {
